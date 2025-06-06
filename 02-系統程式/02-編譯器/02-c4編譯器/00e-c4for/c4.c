8e9b2dc01d10057704266274355e0d289f99e851
jerry92916 <166776207+jerry92916@users.noreply.github.com>
2025-06-06 14:17:41 +0800

    Update c4.c

--- 02-系統程式/02-編譯器/02-c4編譯器/00e-c4for/c4.c
+++ 02-系統程式/02-編譯器/02-c4編譯器/00e-c4for/c4.c
@@ -33,7 +33,7 @@ int *e, *le,  // current position in emitted code (e: 目前機器碼指標, le:
 // tokens and classes (operators last and in precedence order) (按優先權順序排列)
 enum { // token : 0-127 直接用該字母表達， 128 以後用代號。
   Num = 128, Fun, Sys, Glo, Loc, Id,
-  Char, Else, Enum, If, Int, Return, Sizeof, For, While,
+  Char, Else, Enum, If, Int, Return, Sizeof, For, Do, While,
   Assign, Cond, Lor, Lan, Or, Xor, And, Eq, Ne, Lt, Gt, Le, Ge, Shl, Shr, Add, Sub, Mul, Div, Mod, Inc, Dec, Brak
 };
 // opcodes (機器碼的 op)
@@ -59,10 +59,10 @@ void next() // 詞彙解析 lexer
         printf("%d: %.*s", line, p - lp, lp); // 印出該行
         lp = p; // lp = p = 新一行的原始碼開頭
         while (le < e) { // 印出上一行的所有目的碼
-          printf("  %lld: %8.4s", le, &"LLA ,IMM ,JMP ,JSR ,BZ  ,BNZ ,ENT ,ADJ ,LEV ,LI  ,LC  ,SI  ,SC  ,PSH ,"
+          printf("  %d: %8.4s", le+1, &"LLA ,IMM ,JMP ,JSR ,BZ  ,BNZ ,ENT ,ADJ ,LEV ,LI  ,LC  ,SI  ,SC  ,PSH ,"
                            "OR  ,XOR ,AND ,EQ  ,NE  ,LT  ,GT  ,LE  ,GE  ,SHL ,SHR ,ADD ,SUB ,MUL ,DIV ,MOD ,"
                            "OPEN,READ,CLOS,PRTF,MALC,FREE,MSET,MCMP,EXIT,"[*++le * 5]);
-          if (*le <= ADJ) printf("%lld\n", *++le); else printf("\n"); // LLA ,IMM ,JMP ,JSR ,BZ  ,BNZ ,ENT ,ADJ 有一個參數。
+          if (*le <= ADJ) printf("%d\n", *++le); else printf("\n"); // LLA ,IMM ,JMP ,JSR ,BZ  ,BNZ ,ENT ,ADJ 有一個參數。
         }
       }
       ++line;
@@ -311,6 +311,17 @@ void stmt() // 陳述 statement
     }
     *b = (int)(e + 1);
   }
+  else if(tk == Do) { // do stmt while (exp);
+    next();
+    a = e + 1; // a 記住 stmt 開頭
+    stmt();
+    if (tk == While) next(); else { printf("%d: while expected\n", line); exit(-1); }
+    if (tk == '(') next(); else { printf("%d: open paren expected\n", line); exit(-1); }
+    expr(Assign);
+    if (tk == ')') next(); else { printf("%d: close paren expected\n", line); exit(-1); }
+    if (tk == ';') next(); else { printf("%d: semicolon expected\n", line); exit(-1); }
+    *++e = BNZ; *++e = (int)a; // 條件為真時跳回 stmt 開頭
+  }
   else if (tk == While) { // while 語句
     next();
     a = e + 1;
@@ -554,7 +565,7 @@ int compile(int argc, char **argv) // 主程式
   memset(e,    0, poolsz);
   memset(data, 0, poolsz);
 
-  p = "char else enum if int return sizeof for while "
+  p = "char else enum if int return sizeof for do while "
       "open read close printf malloc free memset memcmp exit void main";
   i = Char; while (i <= While) { next(); id[Tk] = i++; } // add keywords to symbol table
   i = OPEN; while (i <= EXIT) { next(); id[Class] = Sys; id[Type] = INT; id[Val] = i++; } // add library to symbol table
